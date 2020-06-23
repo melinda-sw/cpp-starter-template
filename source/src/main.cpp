46bd98d47fdbcc2f67b22feef21364865bc59546
Jan Kelemen <jkeleme@gmail.com>
2020-06-23 15:45:19 +0200

    Add Clang tools support (#3)
    * Add clang-format and clang-tidy tools to the build
    * Install clang-format and clang-tidy on the CI

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,9 +1,5 @@
 #include "../include/main.h"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
-{
-    foobar();
-}
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) { foobar(); }
 
-void foobar() {}
-
+void foobar() { }
