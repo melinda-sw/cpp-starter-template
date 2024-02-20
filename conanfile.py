1ac86aae23bcdabdb4b5e5482e127dea53ead5cb
Jan Kelemen <jkeleme@gmail.com>
2024-02-20 20:49:50 +0100

    Improve Conan integration by adding CMake presets

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,34 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+require_conan_version = ">=2.0"
 
 class CSTConan(ConanFile):
     name = "cst"
     settings = "os", "compiler", "build_type", "arch"
-    build_policy = "missing"
+    version = "0.1"
+
+    exports_sources = "cmake", "src", "CMakeLists.txt", "LICENSE"
 
     def requirements(self):
         self.requires("fmt/10.2.1")
 
     def build_requirements(self):
-        self.test_requires("catch2/3.5.2")
+        self.tool_requires("cmake/[^3.27]")
+        self.test_requires("catch2/[^3.5.2]")
 
     def layout(self):
         cmake_layout(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.user_presets_path = "ConanPresets.json"
         tc.generate()
 
         cmake = CMakeDeps(self)
         cmake.generate()
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
