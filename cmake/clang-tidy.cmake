11999dd1db84f571c2d3a099492c09f17d68411c
Jan Kelemen <jkeleme@gmail.com>
2025-05-08 21:27:09 +0200

    Add Clang MSVC runtime settings, specify all tools in profiles, MSVC compliance

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,12 +1,17 @@
 if (CST_ENABLE_CLANG_TIDY)
     find_program(CLANG_TIDY_EXE NAMES clang-tidy-20 clang-tidy REQUIRED)
     message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
-    
+
+    if (CST_ENABLE_COLORED_COMPILER_OUTPUT)
+        set(COLORED_DIAGNOSTICS -use-color)
+    endif()
+
     set(CMAKE_CXX_CLANG_TIDY
         ${CLANG_TIDY_EXE};
         -format-style='file';
-        -header-filter=${PROJECT_SOURCE_DIR}/src;
-        -extra-arg=-Wno-unknown-warning-option
+        -header-filter=${PROJECT_SOURCE_DIR};
+        -extra-arg=-Wno-unknown-warning-option;
+        ${COLORED_DIAGNOSTICS}
     )
 endif()
 
